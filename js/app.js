bb2dccbdf6d7627f1fc39a81101715165624a43a
CrisRRo <rcristina76@yahoo.com>
2018-07-05 16:00:53 +0300

    Build Enemy function and initialize Enemies' objects

--- js/app.js
+++ js/app.js
@@ -1,7 +1,37 @@
+const rowHeight = 83;				// Height of stone-blocks
+const colWidth = 101;				// Width of stone-blocks
+
+const yForFirstRow = 63;			// Enemy's position on first row
+const enemiesNo = 4;				// Number of enemies
+const allEnemies = [];				// Array to store all enemy objects
+
+const maxSpeed = 450;				// Maximum speed for an enemy
+
+/*	
+*	The 3 possible positions of the enemies on Y axis
+*	(corresponding to each stone-block row)
+*	
+*	Positions of the enemies on y axis will be choose randomly
+*	with getRandomValue() function from the below values
+*/
+const positionsOnYAxis = [
+	yForFirstRow,
+	yForFirstRow + rowHeight,
+	yForFirstRow + 2 * rowHeight
+];
+
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+	/*
+	*	Each enemy has:
+	*	@param x - Random position on x axis outside the canvas
+	*	@param y - Random position on y axis - possible values 
+	*				taken from positionsOnYAxis array
+	*	@param speed - Random speed 
+	*/
+	this.x = - getRandomValue(colWidth);
+	this.y = getRandomValue(positionsOnYAxis);
+	this.speed = getRandomValue(maxSpeed);
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -24,13 +54,32 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.myPlayer = 'images/char-princess-girl.png';
+};
 
+// TODO: Cred ca dt nu are ce cauta aici. Voi vedea.
+// Update the player's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// TODO:
+Player.prototype.handleInput = function() {
+	
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+for (let i = 0; i < enemiesNo; i++) {
+	allEnemies.push(new Enemy());
+}
+
 // Place the player object in a variable called player
-
-
+const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -44,3 +93,12 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// Function to choose a value randomly from an array / interval of numbers
+function getRandomValue(interval) {
+	if (Array.isArray(interval)) {
+		return interval[Math.floor(Math.random() * interval.length)];
+	} else if (typeof interval === "number") {
+		return Math.floor(Math.random() * interval);
+	}
+}
